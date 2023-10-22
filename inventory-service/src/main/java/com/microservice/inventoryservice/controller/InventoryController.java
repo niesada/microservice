c3b68f3ce1c1187ad5ba707618f95410b7b88fd2
Eddie.Nariyoshi <Eddie.Nariyoshi@dendreon.com>
2023-10-21 18:51:50 -0700

    @PathVariable is used
    @GetMapping("/{sku-code}")

--- inventory-service/src/main/java/com/microservice/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/microservice/inventoryservice/controller/InventoryController.java
@@ -1,17 +1,13 @@
 package com.microservice.inventoryservice.controller;
 
-import com.microservice.inventoryservice.dto.InventoryResponse;
 import com.microservice.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -19,10 +15,9 @@ import java.util.List;
 public class InventoryController {
 
   private final InventoryService inventoryService;
-
-  @GetMapping
+  @GetMapping("/{sku-code}")
   @ResponseStatus(HttpStatus.OK)
-  public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
+  public boolean isInStock(@PathVariable("sku-code") String skuCode){
     return inventoryService.isInStock(skuCode);
   }
 }
