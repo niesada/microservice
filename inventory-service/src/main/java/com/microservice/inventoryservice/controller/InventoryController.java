0c39ce65062c92eb35b02420cb3bb530e1bd78c9
Eddie.Nariyoshi <Eddie.Nariyoshi@dendreon.com>
2023-10-21 18:53:04 -0700

    @PathVariable is removed

--- inventory-service/src/main/java/com/microservice/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/microservice/inventoryservice/controller/InventoryController.java
@@ -1,13 +1,17 @@
 package com.microservice.inventoryservice.controller;
 
+import com.microservice.inventoryservice.dto.InventoryResponse;
 import com.microservice.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -15,9 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class InventoryController {
 
   private final InventoryService inventoryService;
-  @GetMapping("/{sku-code}")
+
+  @GetMapping
   @ResponseStatus(HttpStatus.OK)
-  public boolean isInStock(@PathVariable("sku-code") String skuCode){
+  public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
     return inventoryService.isInStock(skuCode);
   }
 }
