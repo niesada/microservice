eafab58a6b16fd1bd25da20825859a1f71e27ee4
Eddie.Nariyoshi <Eddie.Nariyoshi@dendreon.com>
2023-10-21 18:09:27 -0700

    Initial commit

---
+++ order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
@@ -0,0 +1,57 @@
+package com.microservice.orderservice.service;
+
+import com.microservice.orderservice.dto.OrderLineItemsDto;
+import com.microservice.orderservice.dto.OrderRequest;
+import com.microservice.orderservice.model.Order;
+import com.microservice.orderservice.model.OrderLineItems;
+import com.microservice.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+  private final OrderRepository orderRepository;
+  private final WebClient webClient;
+
+  public void placeOrder(OrderRequest orderRequest) {
+    Order order = new Order();
+    order.setOrderNumber(UUID.randomUUID().toString());
+
+    List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+        .stream()
+        .map(this::mapToDto)
+        .toList();
+
+    order.setOrderLineItemsList(orderLineItems);
+
+    // Call Inventory Service, and place order if product is in stock
+    Boolean result = webClient.get()
+        .uri("http://localhost:8082/api/inventory")
+        .retrieve()
+        .bodyToMono(Boolean.class)
+        .block();
+
+    if(result){
+      orderRepository.save(order);
+    } else {
+      throw new IllegalArgumentException("Product is not in stock, please try again later");
+    }
+  }
+
+  private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+    OrderLineItems orderLineItems = new OrderLineItems();
+    orderLineItems.setPrice(orderLineItemsDto.getPrice());
+    orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+    orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+
+    return orderLineItems;
+  }
+}
