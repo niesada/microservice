7abb43f3adb472cc1a6a664e4f8c16180756eed1
iesada nariyoshi <89713944+niesada@users.noreply.github.com>
2023-10-23 10:54:58 -0700

    Multi instances (#2)
    * wip
    
    * wip
    
    * wip
    
    ---------
    
    Co-authored-by: Eddie.Nariyoshi <Eddie.Nariyoshi@dendreon.com>

--- order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
   private final OrderRepository orderRepository;
-  private final WebClient webClient;
+  private final WebClient.Builder webClientBuilder;
 
   public void placeOrder(OrderRequest orderRequest) {
     Order order = new Order();
@@ -37,8 +37,8 @@ public class OrderService {
     List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
 
     // Call Inventory Service, and place order if product is in stock
-    InventoryResponse[] inventoryResponsesArray = webClient.get()
-        .uri("http://localhost:8082/api/inventory",
+    InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
+        .uri("http://inventory-service/api/inventory",
             uriBuilder -> uriBuilder.queryParam("skuCodes", skuCodes).build())
         .retrieve()
         .bodyToMono(InventoryResponse[].class)
