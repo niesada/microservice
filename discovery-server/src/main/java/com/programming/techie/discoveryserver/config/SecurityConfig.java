8f116aa0d1ba059e8da6797488eb38f9e0f8d31d
Eddie.Nariyoshi <Eddie.Nariyoshi@dendreon.com>
2023-10-28 23:00:47 -0700

    wip

--- discovery-server/src/main/java/com/programming/techie/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/programming/techie/discoveryserver/config/SecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
   @Bean
--- order-service/src/main/java/com/microservice/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/microservice/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.microservice.orderservice.controller;
 
 import com.microservice.orderservice.dto.OrderRequest;
 import com.microservice.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
   private final OrderService orderService;
+
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public String placeOrder(@RequestBody OrderRequest orderRequest){
+  @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+  public String placeOrder(@RequestBody OrderRequest orderRequest) {
     orderService.placeOrder(orderRequest);
     return "Order Placed Successfully";
+  }
+
+  public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+    return "Oops! Something went wrong, please order after some time!";
   }
 }
